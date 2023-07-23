81c8a64cec9083fed1835914a397ac73840e9960
Natchnene <kondratsiuk.marina@gmail.com>
2023-07-23 13:27:19 +0300

    linter changes

--- eduplatform/testing_system/apps.py
+++ eduplatform/testing_system/apps.py
@@ -2,5 +2,5 @@ from django.apps import AppConfig
 
 
 class TestingSystemConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'testing_system'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "testing_system"
