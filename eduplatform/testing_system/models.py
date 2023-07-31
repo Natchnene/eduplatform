244f2c9f627240c56252781b66231d24c84fa7f9
Natchnene <kondratsiuk.marina@gmail.com>
2023-07-31 20:52:29 +0300

    create model course, change model group

--- eduplatform/testing_system/models.py
+++ eduplatform/testing_system/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from mentorship.mixins import DateTimeMixin
+from mentorship.models import Specialization, Teacher
 
-# Create your models here.
+
+class Course(models.Model, DateTimeMixin):
+    name = models.CharField(max_length=150)
+    description = models.TextField()
+    teacher = models.ForeignKey(Teacher, on_delete=models.SET_NULL, null=True)
+    specialization = models.ManyToManyField(Specialization, blank=True)
+
+    def __str__(self):
+        return f"{self.pk} - {self.name}"
