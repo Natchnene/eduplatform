85bee5a9b5e1f108875c1b8411caba3fc6991b19
Marina <45893191+Natchnene@users.noreply.github.com>
2023-07-31 20:13:41 +0300

    Models teacher student (#9)
    * create user model
    
    * create manager and mixin
    
    * create migrations and set auth user model
    
    * linter changes
    
    * create models teachet, student, group, specialization

--- eduplatform/mentorship/admin.py
+++ eduplatform/mentorship/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Group, Specialization, Student, Teacher, User
+
+admin.site.register(User)
+admin.site.register(Teacher)
+admin.site.register(Student)
+admin.site.register(Group)
+admin.site.register(Specialization)
--- eduplatform/mentorship/models.py
+++ eduplatform/mentorship/models.py
@@ -29,3 +29,35 @@ class User(AbstractBaseUser, PermissionsMixin, DateTimeMixin):
     class Meta:
         verbose_name = _("user")
         verbose_name_plural = _("users")
+
+
+class Specialization(models.Model, DateTimeMixin):
+    specialization = models.CharField(_("specialization"), max_length=200, blank=True)
+
+    def __str__(self):
+        return f"{self.specialization}"
+
+
+class Teacher(models.Model, DateTimeMixin):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    specialization = models.ManyToManyField(Specialization)
+
+    def __str__(self):
+        return f"{self.pk} - {self.user.email}"
+
+
+class Student(models.Model, DateTimeMixin):
+    rating = models.DecimalField(max_digits=4, decimal_places=2, default=0)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.pk} - {self.user.email}"
+
+
+class Group(models.Model, DateTimeMixin):
+    name = models.CharField(_("name group"), max_length=150)
+    teacher = models.ForeignKey(Teacher, on_delete=models.SET_NULL, null=True)
+    student = models.ManyToManyField(Student, blank=True)
+
+    def __str__(self):
+        return f"{self.pk} - {self.name}"
