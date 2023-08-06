56f2f38e843bbdc4b54b290a5ea703cc58430d1c
Natchnene <kondratsiuk.marina@gmail.com>
2023-08-06 10:53:26 +0300

    create test, question, answer, answertype models, change article model

--- eduplatform/testing_system/models.py
+++ eduplatform/testing_system/models.py
@@ -27,6 +27,37 @@ class Article(models.Model, DateTimeMixin):
     name = models.CharField(max_length=150)
     description = models.TextField()
     course = models.ForeignKey(Course, on_delete=models.SET_NULL, null=True)
+    teacher = models.ForeignKey(Teacher, on_delete=models.SET_NULL, null=True)
 
     def __str__(self):
         return f"{self.pk} - {self.name}"
+
+
+class Test(models.Model, DateTimeMixin):
+    name = models.CharField(max_length=150)
+    description = models.TextField()
+    is_open = models.BooleanField(default=False)
+    time_limit = models.PositiveSmallIntegerField("Time limit in minutes", default=20)
+    topic = models.ForeignKey(Topic, on_delete=models.SET_NULL, null=True)
+    teacher = models.ForeignKey(Teacher, on_delete=models.SET_NULL, null=True)
+
+
+class AnswerType(models.Model, DateTimeMixin):
+    class Types(models.TextChoices):
+        RADIOBUTTON = "radiobutton"
+        CHECKBOX = "checkbox"
+        INPUT = "input"
+
+    type = models.CharField(default="radiobutton", max_length=15, choices=Types.choices)
+
+
+class Question(models.Model, DateTimeMixin):
+    name = models.CharField(max_length=150)
+    test = models.ForeignKey(Test, on_delete=models.SET_NULL, null=True)
+
+
+class Answer(models.Model, DateTimeMixin):
+    text = models.TextField()
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    is_correct = models.BooleanField(default=False)
+    answer_type = models.ForeignKey(AnswerType, on_delete=models.SET_NULL, null=True)
