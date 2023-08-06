e390af787e50d751a32f57101e1daf905906e203
Marina <45893191+Natchnene@users.noreply.github.com>
2023-08-06 10:58:29 +0300

    Test question answer models (#12)
    * create model course, change model group
    
    * create models topic, baseimage
    
    * add pillow
    
    * create article model
    
    * create test, question, answer, answertype models, change article model

--- eduplatform/testing_system/admin.py
+++ eduplatform/testing_system/admin.py
@@ -1,6 +1,11 @@
 from django.contrib import admin
 
-from .models import Course, Topic
+from .models import Answer, AnswerType, Article, Course, Question, Test, Topic
 
 admin.site.register(Course)
 admin.site.register(Topic)
+admin.site.register(Article)
+admin.site.register(Test)
+admin.site.register(AnswerType)
+admin.site.register(Answer)
+admin.site.register(Question)
