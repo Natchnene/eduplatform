818ae1ceaf54b83722ba89695a209cf5434a9d86
Natchnene <kondratsiuk.marina@gmail.com>
2023-09-15 21:36:34 +0300

    create endpoints, urls testing system

--- eduplatform/testing_system/endpoints.py
+++ eduplatform/testing_system/endpoints.py
@@ -1,4 +1,7 @@
+from django.db.models import Subquery
+from mentorship.models import Group
 from rest_framework import permissions, viewsets
+from rest_framework.generics import ListAPIView
 
 from .models import Answer, AnswerType, Article, Course, Question, Test, Topic
 from .serializers import (
@@ -52,3 +55,63 @@ class AnswerViewSet(viewsets.ModelViewSet):
     serializer_class = AnswerSerializer
     queryset = Answer.objects.all()
     permission_classes = [permissions.AllowAny]
+
+
+class CoursesStudentAPIView(ListAPIView):
+    serializer_class = CourseSerializer
+    permission_classes = [permissions.AllowAny]
+
+    def get_queryset(self):
+        student_id = self.kwargs["id"]
+        group = Group.objects.filter(student=student_id)
+        course = Course.objects.filter(id__in=group.values("course_id"))
+        return course
+
+
+class RecommendationAPIView(ListAPIView):
+    serializer_class = CourseSerializer
+    permission_classes = [permissions.AllowAny]
+
+    def get_queryset(self):
+        student_id = self.kwargs["id"]
+        group = Group.objects.filter(student=student_id)
+        student_courses = Course.objects.filter(id__in=Subquery(group.values("course_id")))
+        all_courses = Course.objects.filter(specialization__in=Subquery(student_courses.values("specialization")))
+        recommendation = all_courses.exclude(id__in=Subquery(student_courses.values("id")))
+        return recommendation
+
+
+class CoursesSpecializationAPIView(ListAPIView):
+    serializer_class = CourseSerializer
+    permission_classes = [permissions.AllowAny]
+
+    def get_queryset(self):
+        specialization_id = self.kwargs["id"]
+        return Course.objects.filter(specialization=specialization_id)
+
+
+class QuestionsTestAPIView(ListAPIView):
+    serializer_class = QuestionSerializer
+    permission_classes = [permissions.AllowAny]
+
+    def get_queryset(self):
+        test_id = self.kwargs["id"]
+        return Question.objects.filter(test=test_id)
+
+
+class ArticlesCourseAPIView(ListAPIView):
+    serializer_class = ArticleSerializer
+    permission_classes = [permissions.AllowAny]
+
+    def get_queryset(self):
+        course_id = self.kwargs["id"]
+        return Article.objects.filter(course=course_id)
+
+
+class TestsTeacherAPIView(ListAPIView):
+    serializer_class = TestSerializer
+    permission_classes = [permissions.AllowAny]
+
+    def get_queryset(self):
+        teacher_id = self.kwargs["id"]
+        return Test.objects.filter(teacher=teacher_id)
--- eduplatform/testing_system/urls.py
+++ eduplatform/testing_system/urls.py
@@ -1,14 +1,20 @@
-from django.urls import include, path
+from django.urls import include, path, re_path
 from rest_framework import routers
 
 from .endpoints import (
     AnswerTypeViewSet,
     AnswerViewSet,
     ArticleViewSet,
+    CoursesStudentAPIView,
     CourseViewSet,
     QuestionViewSet,
+    RecommendationAPIView,
     TestViewSet,
     TopicViewSet,
+    CoursesSpecializationAPIView,
+    QuestionsTestAPIView,
+    ArticlesCourseAPIView,
+    TestsTeacherAPIView,
 )
 
 router = routers.SimpleRouter()
@@ -23,4 +29,10 @@ router.register("answerviewset", AnswerViewSet)
 
 urlpatterns = [
     path("", include(router.urls)),
+    re_path("student/(?P<id>[^/.]+)/course", CoursesStudentAPIView.as_view(), name="courses_student"),
+    re_path("student/(?P<id>[^/.]+)/recommendation", RecommendationAPIView.as_view(), name="recommendation_courses"),
+    re_path("specialization/(?P<id>[^/.]+)/courses", CoursesSpecializationAPIView.as_view(), name="courses_specialization"),
+    re_path("test/(?P<id>[^/.]+)/questions", QuestionsTestAPIView.as_view(), name="questions_test"),
+    re_path("course/(?P<id>[^/.]+)/articles", ArticlesCourseAPIView.as_view(), name="articles_course"),
+    re_path("teacher/(?P<id>[^/.]+)/test", TestsTeacherAPIView.as_view(), name="tests_teacher"),
 ]
